91fdf4d21e2e39f513e8f64848bf6ba38fb70c06
Paban09 <masudranapaban22@gmail.com>
2021-06-14 00:49:26 +0600

    Practice php

---
+++ Practice php/First Program.php
@@ -0,0 +1,12 @@
+<html>
+	<head> 
+		<h2> First Php Program </h2>
+	</head>
+<body>
+	
+	<?php
+	   print "Running With Xampp"
+	?>
+
+</body>
+</html>
---
+++ Practice php/secondProgram.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<title> Second program</title>
+	</head>
+	<body>
+	
+		<?php
+			$var = "This is Sum of two variable = ";
+			echo $var;
+			$first=2;
+			$second=3;
+			$sum = $first+$second;
+			echo $sum;
+			
+		?>
+
+	</body>
+</html>
