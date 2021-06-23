c12f8f4c6fe0dd905263c264fc41c674380a8958
Paban09 <masudranapaban22@gmail.com>
2021-06-23 09:28:04 +0600

    Class task (Project)

---
+++ Class task (Project)/Feedback.php
@@ -0,0 +1,31 @@
+<html>
+	<head>
+		<title>Feedback</title>
+	</head>
+	<body>
+		<form>
+			<table border = "2">
+			  <tr>
+				<td align="middle" colspan="2"> Feedback </td>
+			  </tr>
+			  <tr>
+				<td align = "right">Enter Your Userame</td>
+				<td>:<input type "text" placeholder = "Enter Your Userame"></td>
+			  </tr>
+			  
+			  <tr>
+				<td align = "right">Enter Your Opinion</td>
+				<td>:<input type "textarea" placeholder = "Opinion"></td>
+			  </tr>			  			  			  						  			  
+			  <tr>
+				<td colspan ="2">
+					<button>Confirm </button>
+					
+				</td>
+			  </tr>
+			  
+			  
+			</table>
+		</form>
+	</body>
+</html>
---
+++ Class task (Project)/Payment.php
@@ -0,0 +1,53 @@
+<html>
+	<head>
+		<title>Payment</title>
+	</head>
+	<body>
+		<form>
+			<table border = "1">
+			  <tr>
+				<td align="middle" colspan="2"> Payment </td>
+			  </tr>
+			  <tr>
+				<td align = "right">Enter Your Account No</td>
+				<td>:<input type "text" placeholder = "Enter Your Account No "></td>
+			  </tr>
+			  <tr>
+				<td align = "right">Enter Your Password</td>
+				<td>:<input type "password" placeholder = "Enter Your Password "></td>
+			  </tr>
+			  
+			  <tr>
+				<td align = "right">Confirm Password</td>
+				<td>:<input type "password" placeholder = "ConfirmYour Password "></td>
+			  </tr>
+			  
+			  
+				<td  align = "right">Select Payment Method</td>
+				<td>					
+					<input type = "checkbox">Bank Payment<br>
+					<input type = "checkbox">Cash on dilevery<br>
+					<input type = "checkbox">Online paymnet<br>
+										
+				</td>
+				
+			  </tr>
+			  
+			  
+			  <tr>
+				<td colspan ="2">
+					<button>Confirm </button>
+					
+				</td>
+			  </tr>
+			  <tr>
+				<td colspan ="2">
+					<button><a target="_blank" href="Feedback.php" > Feedback  </a> </button>
+					
+				</td>
+			  </tr>
+			  
+			</table>
+		</form>
+	</body>
+</html>
